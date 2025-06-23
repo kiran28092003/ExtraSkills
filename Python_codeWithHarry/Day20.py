6d348001c2183c22837f4b4dfd5f63f9ad28f606
Kiran Tajanpure <129212680+kiran28092003@users.noreply.github.com>
2025-06-23 15:09:04 +0530

    Create Day20.py

---
+++ Python_codeWithHarry/Day20.py
@@ -0,0 +1,20 @@
+x=10
+y=10.11
+z="A"
+name="raj"
+Last_name="sharma"
+print(x)
+print(y)
+print(z)
+print(name)
+print(Last_name)
+print(type(z))
+print(type(y))
+print(type(x))
+print(z,y,z,name,Last_name)                   
+name=input("Enter your name: ")
+Last_name=input("Enter your Last name: ")
+print(f"Welcome to the portal {name} {Last_name}")
+num1=int(input("Enter first num: "))
+num2=int(input("Enter second num: "))
+print(f"The addition of {num1} and {num2} is {num1+num2}")
